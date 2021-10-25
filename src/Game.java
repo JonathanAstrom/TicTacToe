d7361dff344d43aa76c687f3325366745bc1046e
astro <astro@Jontelaptop.lan>
2021-10-25 14:50:14 +0200

    Testar bara

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,8 @@ public class Game {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Testar gruppuppgift");
+		System.out.println("Testar gruppuppgift ");
+		System.out.println("Testar git... ");
 
 	}
 
