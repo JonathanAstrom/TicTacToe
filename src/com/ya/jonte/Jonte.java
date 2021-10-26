20616d6a01029df31197bd34425bd622ae3db0f6
astro <astro@Jontelaptop.lan>
2021-10-26 14:30:22 +0200

    spelplan förslag

--- src/com/ya/jonte/Jonte.java
+++ src/com/ya/jonte/Jonte.java
@@ -3,8 +3,19 @@ package com.ya.jonte;
 public class Jonte {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		char[][] gameBoard = { { '_', '|', '_', '|', '_' }, { '_', '|', '_', '|', '_' }, { ' ', '|', ' ', '|', ' ' }, };//spelplanens uppbyggnad
+		
+		printGameboard(gameBoard);//kallar spelbrädet
+	}
+	
+	public static void printGameboard(char[][] gameBoard) { // printar ut spelbrädet
+		for (char[] row : gameBoard) {
+			for (char i : row) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
 	}
 
 }
